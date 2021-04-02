40c12876571fdc002e142022e100092af9a57b1e
Kat Kuntz <rezonantcheetah@gmail.com>
2021-04-02 09:22:46 -0400

    Add basic nav

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,45 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <header id="header">
+      <div class="logo">Logo / Title</div>
+      <nav id="nav">
+        <router-link :to="{ name: 'Home' }">Home</router-link>
+        <router-link :to="{ name: 'Database' }">Database</router-link>
+        <router-link :to="{ name: 'Login' }">Log In</router-link>
+      </nav>
+    </header>
+    <router-view />
+  </div>
 </template>
 
-<style></style>
+<style>
+#header {
+  display: flex;
+  background-color: lightblue;
+  height: 2rem;
+}
+
+#header .logo {
+  flex-grow: 1;
+  align-self: center;
+  margin-left: 1rem;
+}
+
+#nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  flex-grow: 0.2;
+  margin-right: 1rem;
+}
+
+#nav a {
+  text-decoration: none;
+  padding: 5px 15px;
+}
+
+#nav a.router-link-exact-active {
+  background-color: lightskyblue;
+  align-self: stretch;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,28 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Home from "../views/Home";
+import Database from "../views/Database";
+import Login from "../views/Login";
 
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/database",
+    name: "Database",
+    component: Database,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+];
 
 const router = new VueRouter({
   mode: "history",
